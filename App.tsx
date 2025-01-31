48886fbaa4d9ddd3768a180259662739881552e0
Banjos <48971965+brunosilveiradosanjos@users.noreply.github.com>
2025-01-30 21:45:42 -0300

    feat(setup): expo dev client

--- App.tsx
+++ App.tsx
@@ -1,16 +1,24 @@
-import { StatusBar } from 'react-native';
-import { NativeBaseProvider } from 'native-base';
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { StatusBar } from 'react-native'
+import { NativeBaseProvider } from 'native-base'
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
 
-import { Routes } from './src/routes';
+import { Routes } from './src/routes'
 
-import { THEME } from './src/theme';
-import { Loading } from './src/components/Loading';
+import { THEME } from './src/theme'
+import { Loading } from './src/components/Loading'
 
-import { CartContextProvider } from './src/contexts/CartContext';
+import { CartContextProvider } from './src/contexts/CartContext'
+import { OneSignal } from 'react-native-onesignal'
+
+OneSignal.initialize('cd84abf6-762e-41ed-8354-d4d00b4e0bfe')
+OneSignal.Notifications.requestPermission(true)
 
 export default function App() {
-  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
 
   return (
     <NativeBaseProvider theme={THEME}>
@@ -23,5 +31,5 @@ export default function App() {
         {fontsLoaded ? <Routes /> : <Loading />}
       </CartContextProvider>
     </NativeBaseProvider>
-  );
+  )
 }
