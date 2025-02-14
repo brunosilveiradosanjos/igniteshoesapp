8f381530d857455a2c16fed6bf582f46fd09fe3b
Banjos <brunosilveiradosanjos@gmail.com>
2025-02-13 22:08:11 -0300

    feat(notification): setup push when app is opend

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,38 @@
+import { useEffect, useState } from 'react';
 import { useTheme } from 'native-base';
 import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
+import { NotificationWillDisplayEvent, OneSignal, OSNotification } from 'react-native-onesignal';
+import { Notification } from '../components/Notification';
 
 import { AppRoutes } from './app.routes';
 
 export function Routes() {
+  const [notification, setNotification] = useState<OSNotification>()
   const { colors } = useTheme();
 
   const theme = DefaultTheme;
   theme.colors.background = colors.gray[700];
 
+  useEffect(() => {
+    const handleNotification = (event: NotificationWillDisplayEvent): void => {
+      event.preventDefault()
+      const response = event.getNotification()
+      setNotification(response)
+      console.log(response)
+    }
+
+    OneSignal.Notifications.addEventListener('foregroundWillDisplay', handleNotification)
+    return () => OneSignal.Notifications.removeEventListener('foregroundWillDisplay', handleNotification)
+  }, [])
+
   return (
     <NavigationContainer theme={theme}>
       <AppRoutes />
+      {notification?.title && (
+        <Notification title={notification.title} onClose={() => {
+          setNotification(undefined)
+        }} />
+      )}
     </NavigationContainer>
   );
 }
